81bdbe38ad2af287bf4cc18e4bf0e12b5df5e8c7
shikiishi 　―式井詞― <154775649+shikiishi@users.noreply.github.com>
2024-08-17 14:20:27 +0900

    Update invertColors.js

--- modules/invertColors.js
+++ modules/invertColors.js
@@ -1,33 +1,8 @@
-export default function edgeDetection(imageData) {
-    const width = imageData.width;
-    const height = imageData.height;
-    const data = imageData.data;
-    const edgeData = new Uint8ClampedArray(data.length);
-
-    const kernel = [
-        [1/9 , 1/9 , 1/9],
-        [1/9 , 1/9 , 1/9],
-        [1/9 , 1/9 , 1/9]
-    ];
-
-    for (let y = 1; y < height - 1; y++) {
-        for (let x = 1; x < width - 1; x++) {
-            let r = 0, g = 0, b = 0;
-            for (let ky = -1; ky <= 1; ky++) {
-                for (let kx = -1; kx <= 1; kx++) { // kxは-1から1までの３回の繰り返し
-                    const pixelIndex = ((y + ky) * width + (x + kx)) * 4;
-                    r += data[pixelIndex] * kernel[ky + 1][kx + 1];
-                    g += data[pixelIndex + 1] * kernel[ky + 1][kx + 1];
-                    b += data[pixelIndex + 2] * kernel[ky + 1][kx + 1];
-                }
-            }
-            const index = (y * width + x) * 4;
-            edgeData[index] = r;
-            edgeData[index + 1] = g;
-            edgeData[index + 2] = b;
-            edgeData[index + 3] = data[index + 3]; // alpha
-        }
+export default function invertColors(imageData) {
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      imageData.data[i] = 255 - imageData.data[i]; // red
+      imageData.data[i + 1] = 255 - imageData.data[i + 1]; // green
+      imageData.data[i + 2] = 255 - imageData.data[i + 2]; // blue
     }
-
-    return new ImageData(edgeData, width, height);
-}
+    return imageData;
+  }
